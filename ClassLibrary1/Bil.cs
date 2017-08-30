26301906658c96c53bda1678214ccb6a2f98b1a0
jacob hansen <jacob.aa@hotmail.com>
2017-08-30 11:53:28 +0200

    Tilføjet bil klasse, bil test klasse.

---
+++ ClassLibrary1/Bil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Bil
+    {
+        public string _NummerPlade { get; set; }
+        public DateTime Dato { get; set; }
+
+        public int Pris()
+        {
+            return 240;
+        }
+
+        public string Køretøj()
+        {
+            return "Bil";
+        }
+
+
+
+    }
+}
---
+++ ClassLibrary1Tests/BilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            int pris = bil.Pris();
+            //Assert
+            Assert.AreEqual(240,pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            string køretøj = bil.Køretøj();
+            //Assert
+            Assert.AreEqual("Bil",køretøj);
+        }
+    }
+}
